ec4f43a8e08c7adf6fcc6345cf4701b4f3618f65
pker247 <legendaryphill@hotmail.com>
2019-04-24 23:39:10 -0400

    Current Version of Website
    Buttons working for connecting to database and displaying information.

--- part5/cpanel/cpanel/views.py
+++ part5/cpanel/cpanel/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 import pyrebase
 from django.contrib import auth
-
 config = {
 
     'apiKey': "AIzaSyBJN3d0umh5zKC8OgInDNPlKKEPSUoZlF8",
@@ -10,190 +9,138 @@ config = {
     'projectId': "mesb-70fa5",
     'storageBucket': "mesb-70fa5.appspot.com",
     'messagingSenderId': "196474953476"
-}
+  }
 
 firebase = pyrebase.initialize_app(config)
 
 authe = firebase.auth()
-database = firebase.database()
-session_id = "A"
-
-
+database=firebase.database()
 def signIn(request):
+
     return render(request, "signIn.html")
 
-
 def postsign(request):
-    email = request.POST.get('email')
+    email=request.POST.get('email')
     passw = request.POST.get("pass")
     try:
-        user = authe.sign_in_with_email_and_password(email, passw)
+        user = authe.sign_in_with_email_and_password(email,passw)
     except:
-        message = "invalid credentials"
-        return render(request, "signIn.html", {"messg": message})
-    #print(user['idToken'])
-    session_id = user['idToken']
-    request.session['uid'] = str(session_id)
-    return render(request, "home.html")
-
-
+        message="invalid credentials"
+        return render(request,"signIn.html",{"messg":message})
+    print(user['idToken'])
+    session_id=user['idToken']
+    request.session['uid']=str(session_id)
+    return render(request, "welcome.html",{"e":email})
 def logout(request):
     auth.logout(request)
-    return render(request, 'signIn.html')
+    return render(request,'signIn.html')
 
 
 def signUp(request):
-    return render(request, "signup.html")
 
-
+    return render(request,"signup.html")
 def postsignup(request):
-    name = request.POST.get('name')
-    email = request.POST.get('email')
-    passw = request.POST.get('pass')
+
+    name=request.POST.get('name')
+    email=request.POST.get('email')
+    passw=request.POST.get('pass')
     try:
-        user = authe.create_user_with_email_and_password(email, passw)
+        user=authe.create_user_with_email_and_password(email,passw)
     except:
-        message = "Unable to create account try again"
-        return render(request, "signup.html", {"messg": message})
+        message="Unable to create account try again"
+        return render(request,"signup.html",{"messg":message})
         uid = user['localId']
 
-    data = {"name": name, "status": "1"}
+    data={"name":name,"status":"1"}
 
-    database.child("users").child("details").set(data)
-    return render(request, "signIn.html")
-
+    database.child("users").child(uid).child("details").set(data)
+    return render(request,"signIn.html")
 
 def create(request):
-    return render(request, 'create.html')
+
+    return render(request,'create.html')
 
 
 def post_create(request):
-    return render(request, 'home.html')
 
+    import time
+    from datetime import datetime, timezone
+    import pytz
+
+    tz= pytz.timezone('Asia/Kolkata')
+    time_now= datetime.now(timezone.utc).astimezone(tz)
+    millis = int(time.mktime(time_now.timetuple()))
+    print("mili"+str(millis))
+    work = request.POST.get('work')
+    progress =request.POST.get('progress')
+    idtoken= request.session['uid']
+    a = authe.get_account_info(idtoken)
+    a = a['users']
+    a = a[0]
+    a = a['localId']
+    print("info"+str(a))
+    data = {
+        "work":work,
+        'progress':progress,
+    }
+    database.child('users').child(a).child('reports').child(millis).set(data)
+    name = database.child('users').child(a).child('details').child('name').get().val()
+    return render(request,'welcome.html', {'e':name})
 
 def check(request):
+    import datetime
+    idtoken = request.session['uid']
+    a = authe.get_account_info(idtoken)
+    a = a['users']
+    a = a[0]
+    a = a['localId']
 
-    idtoken = session_id
+    timestamps = database.child('users').child(a).child('reports').shallow().get().val()
+    lis_time=[]
+    for i in timestamps:
 
-    runID = list()
-    runs = list()
-    durations = list()
-    peripherals = list()
+        lis_time.append(i)
 
-    runID = database.child('runs').shallow().get().val()
-    runID = list(runID)
-    for i in runID:
-        runs.append(database.child('runs/' + i).get().val())
-        durations.append(database.child('runs/' + i + '/duration').get().val())
-        peripherals.append(database.child('runs/' + i + '/peripherals').shallow().get().val())
-    #print(runID)
-    #print(runs)
-    #print(durations)
-    #print(peripherals)
-    #print(functions)
-    #print(vars)
-    #comb_lis = zip(runID, durations)
-    return render(request, 'check.html', {'RID': runID})
+    lis_time.sort(reverse=True)
 
-def curr_check(request):
+    print(lis_time)
+    work = []
 
-    idtoken = session_id
+    for i in lis_time:
 
-    runID = list()
-    runs = list()
-    durations = list()
-    peripherals = list()
+        wor=database.child('users').child(a).child('reports').child(i).child('work').get().val()
+        work.append(wor)
+    print(work)
 
-    runID = database.child('runs').shallow().get().val()
-    runID = list(runID)[-1]
-    temp = ""
-    for x in runID:
-        temp+=x
-    return render(request, 'curr_check.html', {'RID': temp})
+    date=[]
+    for i in lis_time:
+        i = float(i)
+        dat = datetime.datetime.fromtimestamp(i).strftime('%H:%M %d-%m-%Y')
+        date.append(dat)
 
-def post_curr_check(request):
+    print(date)
 
-    run = request.GET.get('a')
-    peripherals = database.child('runs/' + run + '/peripherals').shallow().get().val()
-    peripherals = list(peripherals)
-    names = list()
-    vars = list()
-    vars_names = list()
-    vars_size = list()
-    vars_type = list()
-    vals = list()
-    val_keys = list()
-    val_vals = list()
-    temp2 = list()
-    for x in range(len(peripherals)):
-        names.append(database.child('runs/' + run + '/peripherals/' + peripherals[x] + '/name').get().val())
-        temp = list(database.child('runs/' + run + '/peripherals/' + peripherals[x] + '/vars').shallow().get().val())
-        vars.append(temp[x])
+    comb_lis = zip(lis_time,date,work)
+    name = database.child('users').child(a).child('details').child('name').get().val()
 
-        for y in range(len(vars)):
-            vars_names.append(database.child(
-                'runs/' + run + '/peripherals/' + peripherals[x] + '/vars/' + vars[y] + '/name').get().val())
-            vars_size.append(database.child(
-                'runs/' + run + '/peripherals/' + peripherals[x] + '/vars/' + vars[y] + '/size').get().val())
-            vars_type.append(database.child(
-                'runs/' + run + '/peripherals/' + peripherals[x] + '/vars/' + vars[y] + '/type').get().val())
-            vals.append(database.child(
-                'runs/' + run + '/peripherals/' + peripherals[x] + '/vars/' + vars[y] + '/val').get().val())
-
-            for aval in vals:
-                temp2.append(sorted(aval.items()))
-
-    #for v in vars:
-    #   if v =
-    print(temp2)
-    comb_list = zip(names, peripherals)
-    comb_list2 = zip(vars_names, vars, vars_size, vars_type, temp2)
-    comb_list3 = zip(val_keys, val_vals)
-    return render(request, 'post_curr_check.html', {'comb_lis': comb_list, 'comb_lis2': comb_list2, 'run': run})
-
+    return render(request,'check.html',{'comb_lis':comb_lis,'e':name})
 
 def post_check(request):
 
-    run = request.GET.get('z')
-    peripherals = database.child('runs/' + run + '/peripherals').shallow().get().val()
-    peripherals = list(peripherals)
-    names = list()
-    vars = list()
-    vars_names = list()
-    vars_size = list()
-    vars_type = list()
-    vals = list()
-    val_keys = list()
-    val_vals = list()
-    for x in range(len(peripherals)):
-        names.append(database.child('runs/' + run + '/peripherals/' + peripherals[x] + '/name').get().val())
-        temp = list(database.child('runs/' + run + '/peripherals/' + peripherals[x] + '/vars').shallow().get().val())
-        vars.append(temp[x])
+    import datetime
 
-        for y in range(len(vars)):
-            vars_names.append(database.child('runs/' + run + '/peripherals/' + peripherals[x] + '/vars/' + vars[y] + '/name').get().val())
-            vars_size.append(database.child('runs/' + run + '/peripherals/' + peripherals[x] + '/vars/' + vars[y] + '/size').get().val())
-            vars_type.append(database.child('runs/' + run + '/peripherals/' + peripherals[x] + '/vars/' + vars[y] + '/type').get().val())
-            vals.append(database.child('runs/' + run + '/peripherals/' + peripherals[x] + '/vars/' + vars[y] + '/val').get().val())
+    time = request.GET.get('z')
 
+    idtoken = request.session['uid']
+    a = authe.get_account_info(idtoken)
+    a = a['users']
+    a = a[0]
+    a = a['localId']
 
-            for key, value in vals[y].items():
-                val_keys.append(key)
-                val_vals.append(value)
+    work =database.child('users').child(a).child('reports').child(time).child('work').get().val()
+    progress =database.child('users').child(a).child('reports').child(time).child('progress').get().val()
+    i = float(time)
+    dat = datetime.datetime.fromtimestamp(i).strftime('%H:%M %d-%m-%Y')
+    name = database.child('users').child(a).child('details').child('name').get().val()
 
-    print(vals)
-    comb_list = zip(names, peripherals)
-    comb_list2 = zip(vars_names, vars, vars_size, vars_type, vals)
-    comb_list3 = zip(val_keys, val_vals)
-    return render(request, 'post_check.html', {'comb_lis': comb_list, 'comb_lis2': comb_list2, 'run': run})
-
-
-def post_check2(request):
-
-    run = request.GET.get('z')
-    peri = request.GET.get('y')
-    print(run)
-    vars = database.child('runs/' + run + '/peripherals').shallow().get().val()
-    print(vars)
-    #{'name': name}
-    return render(request, 'post_check2.html')
+    return render(request,'post_check.html',{'w':work,'p':progress,'d':dat,'e':name})
